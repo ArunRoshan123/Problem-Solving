e791d983784792c1e60f1b43743e686519268d9b
A.R. Arun Roshan <arunraksha234@gmail.com>
2023-03-18 00:16:41 +0530

    Create Selection sort.cpp

---
+++ Selection sort.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+void selectionSort(vector<int>& arr, int n)
+{   
+    // Write your code here.
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                swap(arr[j],arr[i]);
+            }
+        }
+    }
+}
